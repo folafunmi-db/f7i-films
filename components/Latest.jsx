45a02d17f75796cf127c2a7a8000dfa2a94666f0
Folafunmi <folafunmimustapha@gmail.com>
2024-08-03 00:49:04 +0100

    style: update link widths

--- components/Latest.jsx
+++ components/Latest.jsx
@@ -5,19 +5,19 @@ import { useState, useEffect } from "react";
 import { useSession } from "next-auth/client";
 
 const Container = styled.div`
-	background-size: cover;
-	background-position: center;
-	background: #7c3aed;
-	min-height: 24rem;
-	${tw`shadow-xl rounded-lg w-full bg-center text-white my-4 bg-cover flex flex-col sm:flex-row justify-between items-center px-6 py-4 gap-4`}
+  background-size: cover;
+  background-position: center;
+  background: #7c3aed;
+  min-height: 24rem;
+  ${tw`shadow-xl rounded-lg w-full bg-center text-white my-4 bg-cover flex flex-col sm:flex-row justify-between items-center px-6 py-4 gap-4`}
 `;
 
 const Pic = styled.div`
-	min-height: 20rem;
-	${({ image, url }) =>
-		image &&
-		`background-image: url("https://image.tmdb.org/t/p/w500/${image}");`};
-	${tw` w-full sm:w-2/5 bg-cover bg-center rounded-lg cursor-pointer`}
+  min-height: 20rem;
+  ${({ image, url }) =>
+    image &&
+    `background-image: url("https://image.tmdb.org/t/p/w500/${image}");`};
+  ${tw` w-full bg-cover bg-center rounded-lg cursor-pointer`}
 `;
 
 const Group = tw.div`flex flex-col w-full gap-3 sm:w-3/5`;
@@ -25,65 +25,63 @@ const Group = tw.div`flex flex-col w-full gap-3 sm:w-3/5`;
 const Title = tw.h2`text-2xl font-bold text-yellow-400 whitespace-nowrap cursor-pointer overflow-x-hidden overflow-ellipsis`;
 
 const Rating = styled.h2`
-	${tw`text-xl flex w-max justify-center items-center font-bold gap-1`};
+  ${tw`text-xl flex w-max justify-center items-center font-bold gap-1`};
 `;
 
 const Overview = styled.div`
-	display: -webkit-box;
-	-webkit-line-clamp: 5;
-	-webkit-box-orient: vertical;
-	overflow: hidden;
+  display: -webkit-box;
+  -webkit-line-clamp: 5;
+  -webkit-box-orient: vertical;
+  overflow: hidden;
 `;
 
 const WatchList = tw.button` focus:outline-none active:bg-yellow-300 outline-none font-medium bg-yellow-400 hover:shadow-lg px-2 w-max py-4 rounded-lg font-m flex justify-between items-center text-purple-700`;
 
 const useStateWithLocalStorage = (localStorageKey) => {
-	const [value, setValue] = useState(
-		localStorage.getItem(localStorageKey) || false
-	);
+  const [value, setValue] = useState(
+    localStorage.getItem(localStorageKey) || false
+  );
 
-	useEffect(() => {
-		if (value) {
-			localStorage.setItem(localStorageKey, value);
-		} else {
-			localStorage.removeItem(localStorageKey);
-		}
-	}, [value]);
+  useEffect(() => {
+    if (value) {
+      localStorage.setItem(localStorageKey, value);
+    } else {
+      localStorage.removeItem(localStorageKey);
+    }
+  }, [value]);
 
-	return [value, setValue];
+  return [value, setValue];
 };
 
 const Latest = ({ image, url, title, vote, id, overview, isLoading }) => {
-	const [session, loading] = useSession();
-	const [added, setAdded] = useStateWithLocalStorage(`Movie${id}`);
+  const [session, loading] = useSession();
+  const [added, setAdded] = useStateWithLocalStorage(`Movie${id}`);
 
-	return (
-		<Container>
-			<Link href={`/movie/${id}`}>
-				<Pic image={isLoading ? false : image}></Pic>
-			</Link>
-			<Group>
-				<Link href={`/movie/${id}`}>
-					<Title>{title}</Title>
-				</Link>
-				<Rating>
-					<BiStar style={{ color: "#fbbf24" }} />
-					{vote}
-				</Rating>
-				{loading ? <Overview>Hold on...</Overview> : ""}
-				{session ? (
-					<WatchList onClick={() => setAdded(!added)}>
-						{added || false
-							? "Added to Watchlist"
-							: "+ Add to Watchlist"}
-					</WatchList>
-				) : (
-					""
-				)}
-				<Overview>{overview}</Overview>
-			</Group>
-		</Container>
-	);
+  return (
+    <Container>
+      <Link href={`/movie/${id}`} style={{ width: "50%" }}>
+        <Pic image={isLoading ? false : image}></Pic>
+      </Link>
+      <Group>
+        <Link href={`/movie/${id}`}>
+          <Title>{title}</Title>
+        </Link>
+        <Rating>
+          <BiStar style={{ color: "#fbbf24" }} />
+          {vote}
+        </Rating>
+        {loading ? <Overview>Hold on...</Overview> : ""}
+        {session ? (
+          <WatchList onClick={() => setAdded(!added)}>
+            {added || false ? "Added to Watchlist" : "+ Add to Watchlist"}
+          </WatchList>
+        ) : (
+          ""
+        )}
+        <Overview>{overview}</Overview>
+      </Group>
+    </Container>
+  );
 };
 
 export default Latest;
--- components/Movie.jsx
+++ components/Movie.jsx
@@ -3,39 +3,39 @@ import { BiStar } from "react-icons/bi";
 import Link from "next/link";
 
 const Container = styled.div`
-	/* background-color: #fffaa0;
+  /* background-color: #fffaa0;
 	background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='100%25' height='100%25' viewBox='0 0 1200 800'%3E%3Cdefs%3E%3CradialGradient id='a' cx='0' cy='800' r='800' gradientUnits='userSpaceOnUse'%3E%3Cstop offset='0' stop-color='%23ffc68c'/%3E%3Cstop offset='1' stop-color='%23ffc68c' stop-opacity='0'/%3E%3C/radialGradient%3E%3CradialGradient id='b' cx='1200' cy='800' r='800' gradientUnits='userSpaceOnUse'%3E%3Cstop offset='0' stop-color='%23ffa4aa'/%3E%3Cstop offset='1' stop-color='%23ffa4aa' stop-opacity='0'/%3E%3C/radialGradient%3E%3CradialGradient id='c' cx='600' cy='0' r='600' gradientUnits='userSpaceOnUse'%3E%3Cstop offset='0' stop-color='%23ff90c8'/%3E%3Cstop offset='1' stop-color='%23ff90c8' stop-opacity='0'/%3E%3C/radialGradient%3E%3CradialGradient id='d' cx='600' cy='800' r='600' gradientUnits='userSpaceOnUse'%3E%3Cstop offset='0' stop-color='%23fffaa0'/%3E%3Cstop offset='1' stop-color='%23fffaa0' stop-opacity='0'/%3E%3C/radialGradient%3E%3CradialGradient id='e' cx='0' cy='0' r='800' gradientUnits='userSpaceOnUse'%3E%3Cstop offset='0' stop-color='%23ff8077'/%3E%3Cstop offset='1' stop-color='%23ff8077' stop-opacity='0'/%3E%3C/radialGradient%3E%3CradialGradient id='f' cx='1200' cy='0' r='800' gradientUnits='userSpaceOnUse'%3E%3Cstop offset='0' stop-color='%23f9a9ff'/%3E%3Cstop offset='1' stop-color='%23f9a9ff' stop-opacity='0'/%3E%3C/radialGradient%3E%3C/defs%3E%3Crect fill='url(%23a)' width='1200' height='800'/%3E%3Crect fill='url(%23b)' width='1200' height='800'/%3E%3Crect fill='url(%23c)' width='1200' height='800'/%3E%3Crect fill='url(%23d)' width='1200' height='800'/%3E%3Crect fill='url(%23e)' width='1200' height='800'/%3E%3Crect fill='url(%23f)' width='1200' height='800'/%3E%3C/svg%3E");
 	background-attachment: fixed; */
-	background-size: cover;
-	background-position: center;
-	/* background: rgba(255, 255, 255, 0.2); */
-	/* backdrop-filter: blur(40px); */
-	/* border: 3px solid transparent; */
-	/* background-clip: padding-box; */
-	background-color: #7c3aed;
-	height: 28rem;
+  background-size: cover;
+  background-position: center;
+  /* background: rgba(255, 255, 255, 0.2); */
+  /* backdrop-filter: blur(40px); */
+  /* border: 3px solid transparent; */
+  /* background-clip: padding-box; */
+  background-color: #7c3aed;
+  height: 28rem;
 
-	${tw`shadow-lg hover:shadow-xl transition text-white font-m rounded-lg w-full flex flex-col justify-center items-center`}
+  ${tw`shadow-lg hover:shadow-xl transition text-white font-m rounded-lg w-full flex flex-col justify-center items-center`}
 `;
 
 const Pic = styled.div`
-	height: 24rem;
-	${({ image, url }) =>
-		image &&
-		`background-image: url("https://image.tmdb.org/t/p/w500/${image}");`};
-	${tw` w-11/12 bg-cover bg-center rounded-lg cursor-pointer`}
-	transition: .1s ease-in;
+  height: 24rem;
+  ${({ image, url }) =>
+    image &&
+    `background-image: url("https://image.tmdb.org/t/p/w500/${image}");`};
+  ${tw` w-11/12 mx-auto bg-cover bg-center rounded-lg cursor-pointer`}
+  transition: .1s ease-in;
 
-	&:hover {
-		transform: scaleX(1.09) scaleY(1.07);
-		transition: 0.1s ease-in;
-	}
+  &:hover {
+    transform: scaleX(1.09) scaleY(1.07);
+    transition: 0.1s ease-in;
+  }
 `;
 
 const Group = styled.div`
-	height: 3rem;
-	width: 100%;
-	${tw` w-11/12 flex justify-between items-center gap-6 `}
+  height: 3rem;
+  width: 100%;
+  ${tw` w-11/12 flex justify-between items-center gap-1 `}
 `;
 
 const Title = tw.h2`text-base whitespace-nowrap overflow-x-hidden overflow-ellipsis`;
@@ -43,22 +43,22 @@ const Title = tw.h2`text-base whitespace-nowrap overflow-x-hidden overflow-ellip
 const Rating = tw.h2`text-xl flex justify-center text-yellow-400 items-center gap-1`;
 
 const Movie = ({ image, url, title, vote, id }) => {
-	return (
-		<Container>
-			<Link href={`/movie/${id}`}>
-				<Pic image={image} url={url}></Pic>
-			</Link>
-			<Group>
-				<Link href={`/movie/${id}`}>
-					<Title>{title}</Title>
-				</Link>
-				<Rating>
-					<BiStar style={{color: "#fbbf24"}}/>
-					{vote}
-				</Rating>
-			</Group>
-		</Container>
-	);
+  return (
+    <Container>
+      <Link href={`/movie/${id}`} style={{ width: "100%" }}>
+        <Pic image={image} url={url}></Pic>
+      </Link>
+      <Group>
+        <Link href={`/movie/${id}`} style={{ width: "60%" }}>
+          <Title>{title}</Title>
+        </Link>
+        <Rating>
+          <BiStar style={{ color: "#fbbf24" }} />
+          {vote}
+        </Rating>
+      </Group>
+    </Container>
+  );
 };
 
 export default Movie;
