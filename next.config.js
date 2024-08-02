f7d73a13a6c4f5c864223caf5bff8ab9771b3fc0
Folafunmi <folafunmimustapha@gmail.com>
2024-08-03 00:32:45 +0100

    feat: upgrade packages

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
 module.exports = {
-  webpack: (config, { isServer }) => {
-    // Fixes packages that depend on fs/module module
-    if (!isServer) {
-      config.node = { fs: 'empty', module: 'empty' }
-    }
+  // webpack: (config, { isServer }) => {
+  //   // Fixes packages that depend on fs/module module
+  //   if (!isServer) {
+  //     config.node = { fs: 'empty', module: 'empty' }
+  //   }
+  //   return config
+  // },
+};
 
-    return config
-  },
-}
